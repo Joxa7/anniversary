eb528e4dd59b7b94d3d390587d7b8adf73f24404
Joxa7 <70818809+Joxa7@users.noreply.github.com>
2024-01-06 18:11:49 +0100

    Create script.js

---
+++ landing/script.js
@@ -0,0 +1,73 @@
+
+
+var basicTimeline = anime.timeline({
+  autoplay: false
+});
+
+var pathEls = $(".check");
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute("stroke-dashoffset", offset);
+}
+
+basicTimeline
+  .add({
+    targets: ".text",
+    duration: 1,
+    opacity: "0"
+  })
+  .add({
+    targets: ".button",
+    duration: 1300,
+    height: 10,
+    width: 300,
+    backgroundColor: "#FFFFFF",
+    border: "0",
+    borderRadius: 100
+  })
+  .add({
+    targets: ".progress-bar",
+    duration: 2000,
+    width: 300,
+    easing: "linear"
+  })
+  .add({
+    targets: ".button",
+    width: 0,
+    duration: 1
+  })
+  .add({
+    targets: ".progress-bar",
+    width: 80,
+    height: 80,
+    delay: 500,
+    duration: 750,
+    borderRadius: 80,
+    backgroundColor: "#FFFFFF"
+  })
+  .add({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    duration: 200,
+    easing: "easeInOutSine"
+  });
+
+$(".button").click(function() {
+  basicTimeline.play();
+});
+
+$(".text").click(function() {
+  basicTimeline.play();
+});
+
+basicTimeline.complete = function() {
+  setTimeout(function() {
+    window.location.href = 'index.html'; // Redirect to index.html after a 2-second delay
+  }, 500); // Delay of 2000 milliseconds (2 seconds)
+};
+
+
+$(".text").click(function() {
+basicTimeline.play();
+});
